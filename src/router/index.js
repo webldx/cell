8770e812452a437f138e874e9df05dd436db455a
lidaxuan <15201002062@163.com>
2018-09-03 14:23:15 +0800

    初始化仓库与代码规范

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/views/Login';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    }
+  ]
+});
