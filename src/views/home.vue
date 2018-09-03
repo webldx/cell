3cb07ca39d8972427126226fc32bd62e06b6a49b
lidaxuan <15201002062@163.com>
2018-09-03 21:49:06 +0800

    home页的头部

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
-  </div>
+  <el-container>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="">
+        </el-col>
+        <el-col class="middle" :span="19">
+          <span>电商后台管理项目</span>
+        </el-col>
+        <el-col :span="1">
+          <a class="logout" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+  </el-container>
 </template>
 
 <script>
@@ -17,8 +23,19 @@ export default {
 </script>
 
 <style>
+.middle {
+  text-align: center;
+  font-size: 24px;
+  color: #fff;
+}
+
+.logout {
+  text-decoration: none;
+  color: orange;
+}
+
 .el-container {
-  height: 100%; 
+  height: 100%;
 }
 
 .el-header, .el-footer {
@@ -26,6 +43,7 @@ export default {
   color: #333;
   text-align: center;
   line-height: 60px;
+  padding: 0;
 }
 
 .el-aside {
