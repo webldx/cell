4a5e222c0af3dcfd1e810c8f6fc81c7f1ed73c52
lidaxuan <15201002062@163.com>
2018-09-05 00:18:15 +0800

    表格样式初步搭建

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -16,12 +16,39 @@
         <el-button type="success" plain>添加用户</el-button>
       </el-col>
     </el-row>
+    <el-table :data="tableData" border stripe style="width: 100%">
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="name" label="电话" width="180"></el-table-column>
+      <el-table-column prop="address" label="用户状态"></el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    };
+  }
 };
 </script>
 
