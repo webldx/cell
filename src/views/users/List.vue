831c50fa3ad2da5a410a8855840d4ab5b1a13613
lidaxuan <15201002062@163.com>
2018-09-05 19:55:36 +0800

    根据文档渲染操作列的按钮

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -18,12 +18,18 @@
     </el-row>
     <el-table :data="tableData" border stripe style="width: 100%">
       <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
-      <el-table-column prop="mobile" label="电话" width="180"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="100"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="150"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="150"></el-table-column>
       <el-table-column prop="create_time" label="时间"></el-table-column>
       <el-table-column prop="mg_state" label="用户状态"></el-table-column>
-      <el-table-column label="操作"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
+          <el-button type="success" icon="el-icon-check" plain size="mini"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
