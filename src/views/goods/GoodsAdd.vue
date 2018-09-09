024b53ca68759d922623688808334e66e721f2ff
lidaxuan <15201002062@163.com>
2018-09-10 01:26:17 +0800

    展示静态资源

--- src/views/goods/GoodsAdd.vue
+++ src/views/goods/GoodsAdd.vue
@@ -55,7 +55,11 @@
             </el-checkbox-group>
           </el-form-item>
         </el-tab-pane>
-        <el-tab-pane label="商品属性"></el-tab-pane>
+        <el-tab-pane label="商品属性">
+          <el-form-item v-for="item in staticParams" :key="item.attr_id" :label="item.attr_name">
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
         <el-tab-pane label="商品图片">商品图片</el-tab-pane>
         <el-tab-pane label="商品内容">商品内容</el-tab-pane>
       </el-tabs>
